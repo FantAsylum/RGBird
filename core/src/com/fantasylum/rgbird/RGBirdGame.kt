e3855d53e6a09a191f85de9eb1a0c3af0aaef50f
PatriotCodes <patriot130690@gmail.com>
2017-12-04 20:38:18 +0200

    green bird texture fix

--- core/src/com/fantasylum/rgbird/RGBirdGame.kt
+++ core/src/com/fantasylum/rgbird/RGBirdGame.kt
@@ -3,14 +3,18 @@ package com.fantasylum.rgbird
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.fantasylum.screens.GameScreen
 import com.fantasylum.utils.MyStack
 
 class RGBirdGame : ApplicationAdapter() {
 
+	private val assetManager = AssetManager()
 	private val screenStack = MyStack<Screen>()
 
 	override fun create () {
+		setResourcesToBeLoaded()
 		screenStack.push(GameScreen())
 	}
 
@@ -31,4 +35,13 @@ class RGBirdGame : ApplicationAdapter() {
 		screenStack.push(screen)
 	}
 
+	private fun setResourcesToBeLoaded() {
+		assetManager.apply {
+			load("graphics/atlas.atlas", TextureAtlas::class.java)
+		}
+	}
+
+	override fun dispose() {
+		assetManager.dispose()
+	}
 }
