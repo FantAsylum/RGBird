12e08c35f7a5c1842eb12b79de74f704cdf0915e
PatriotCodes <patriot130690@gmail.com>
2017-12-05 22:19:12 +0200

    atlas created

--- core/src/com/fantasylum/utils/TexturesLoader.kt
+++ core/src/com/fantasylum/utils/TexturesLoader.kt
@@ -2,11 +2,12 @@ package com.fantasylum.utils
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 
-class TexturesLoader() {
+class TexturesLoader {
     private val textureAtlas = TextureAtlas(Gdx.files.internal("graphics/atlas.atlas"))
 
-    val birdRedTexture = textureAtlas.findRegion("bird_red").texture
+    val birdRedTexture : TextureRegion = textureAtlas.findRegion("bird_red")
 
     private object Holder { val INSTANCE = TexturesLoader() }
 
