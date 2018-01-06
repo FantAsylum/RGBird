5845aae2ac04ac61934a1f0cd3176dcbadf98b63
dan <takahawkkun@gmail.com>
2018-01-06 14:39:22 +0200

    Working on menu UI

---
+++ core/src/io/github/fantasylum/rgbot/resid/ResId.kt
@@ -0,0 +1,3 @@
+package io.github.fantasylum.rgbot.resid
+
+val UI_SKIN = "skins/neon_ui"
--- core/src/io/github/fantasylum/rgbot/screens/MenuScreen.kt
+++ core/src/io/github/fantasylum/rgbot/screens/MenuScreen.kt
@@ -1,5 +1,26 @@
 package io.github.fantasylum.rgbot.screens
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Table
 
-class MenuScreen : ScreenAdapter()
+import io.github.fantasylum.rgbot.resid.*
+import io.github.fantasylum.rgbot.RGBot
+
+class MenuScreen : ScreenAdapter() {
+    private val stage = Stage()
+    private val table = Table()
+    private val title = Label("Stub", RGBot.getSkin(UI_SKIN))
+
+    init {
+        table.setFillParent(true)
+        table.add(title)
+        stage.addActor(table)
+    }
+
+    override fun show() {
+        Gdx.input.inputProcessor = stage
+    }
+}
