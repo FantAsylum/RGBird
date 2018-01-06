c0a0851c735b8615bd76d691651f7111ecab9b72
dan <takahawkkun@gmail.com>
2018-01-06 14:38:56 +0200

    added routines for managing assets

--- core/src/io/github/fantasylum/rgbot/RGBot.kt
+++ core/src/io/github/fantasylum/rgbot/RGBot.kt
@@ -5,11 +5,14 @@ import java.util.LinkedList
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 
 import io.github.fantasylum.rgbot.screens.MenuScreen
 
 object RGBot : ApplicationAdapter() {
     private val screenStack = LinkedList<Screen>()
+    private val assetManager = AssetManager()
 
     override fun create() {
         pushScreen(MenuScreen())
@@ -17,6 +20,10 @@ object RGBot : ApplicationAdapter() {
 
     override fun render() {
         screenStack.peek().render(Gdx.graphics.deltaTime)
+    }
+
+    override fun dispose() {
+        assetManager.dispose()
     }
 
     fun pushScreen(screen: Screen) {
@@ -32,5 +39,11 @@ object RGBot : ApplicationAdapter() {
     fun replaceScreen(screen: Screen) {
         screenStack.pop()
         screenStack.push(screen)
+    }
+
+    fun getSkin(id: String): Skin {
+        assetManager.load(id, Skin::class.java)
+        assetManager.finishLoading()
+        return assetManager.get(id)
     }
 }
