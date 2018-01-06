24226b92ecd417d697037a7d6fd2d7b4abcf8a02
dan <takahawkkun@gmail.com>
2018-01-06 13:18:19 +0200

    Added basic screen managing routine

--- core/src/io/github/fantasylum/rgbot/RGBot.kt
+++ core/src/io/github/fantasylum/rgbot/RGBot.kt
@@ -1,5 +1,36 @@
 package io.github.fantasylum.rgbot
 
-import com.badlogic.gdx.ApplicationAdapter
+import java.util.LinkedList
 
-object RGBot : ApplicationAdapter()
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+
+import io.github.fantasylum.rgbot.screens.MenuScreen
+
+object RGBot : ApplicationAdapter() {
+    private val screenStack = LinkedList<Screen>()
+
+    override fun create() {
+        pushScreen(MenuScreen())
+    }
+
+    override fun render() {
+        screenStack.peek().render(Gdx.graphics.deltaTime)
+    }
+
+    fun pushScreen(screen: Screen) {
+        screenStack.push(screen)
+    }
+
+    fun popScreen() {
+        screenStack.pop()
+        if (screenStack.isEmpty())
+            Gdx.app.exit()
+    }
+
+    fun replaceScreen(screen: Screen) {
+        screenStack.pop()
+        screenStack.push(screen)
+    }
+}
---
+++ core/src/io/github/fantasylum/rgbot/screens/MenuScreen.kt
@@ -0,0 +1,5 @@
+package io.github.fantasylum.rgbot.screens
+
+import com.badlogic.gdx.ScreenAdapter
+
+class MenuScreen : ScreenAdapter()
