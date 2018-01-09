f4a61ee87b25b027d0c08174d89d6da6b4b4e45b
Dan Hawk <takahawkkun@gmail.com>
2018-01-09 21:40:53 +0200

    Added screen clearing before rendering

--- core/src/io/github/fantasylum/rgbot/RGBot.kt
+++ core/src/io/github/fantasylum/rgbot/RGBot.kt
@@ -4,6 +4,7 @@ import java.util.LinkedList
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
@@ -19,6 +20,8 @@ object RGBot : ApplicationAdapter() {
     }
 
     override fun render() {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
         screenStack.peek().render(Gdx.graphics.deltaTime)
     }
 
